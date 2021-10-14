4154c5bb0701f97b8955d0a0315a45c871f0b776
Matej Višnjić <visnjicmatej@gmail.com>
2021-10-14 22:19:20 +0200

    Dodan tailwindcss lokalno kako bi promjenio font, izmjenjen dizajn stranice

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        alfa: ["'Alfa Slab One' , 'cursive'"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
