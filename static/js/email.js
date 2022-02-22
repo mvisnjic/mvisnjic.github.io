e2adf9d997998e140a059883b5484ebdba629d02
Matej Višnjić <visnjicmatej@gmail.com>
2022-02-23 00:33:35 +0100

    update: font on about me, -working on contact windows hidden after sending email

--- static/js/email.js
+++ static/js/email.js
@@ -6,9 +6,14 @@ document
   .getElementById("contactForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-
     const serviceID = "service_yd0nwab";
     const templateID = "template_gwncmd2";
+    document.getElementById("sendBtn").hidden = true;
+    document.getElementById("contactFormWindow").hidden = true;
+    document.getElementById("email").value = "";
+    document.getElementById("fullname").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
 
     emailjs.sendForm(serviceID, templateID, this).then(
       (response) => {
@@ -16,15 +21,16 @@ document
         alert(
           "Uspješno poslan email!\nOdgovoriti ću Vam što brže je moguće.\nHvala :)"
         );
-        document.getElementById("email").value = "";
-        document.getElementById("fullname").value = "";
-        document.getElementById("subject").value = "";
-        document.getElementById("message").value = "";
+        //document.getElementById("sendBtn").disabled = true;
+        // document.getElementById("email").value = "";
+        // document.getElementById("fullname").value = "";
+        // document.getElementById("subject").value = "";
+        // document.getElementById("message").value = "";
       },
       (error) => {
         console.log("Greška!", error);
         alert(
-          "Greška: Mail nije poslan. \nUkoliko se problem pojavljuje kontaktirajte me direktno na mail: \nvisnjicmatej@gmail.com"
+          "Greška: Mail nije poslan! \nUkoliko se problem opet pojavljuje kontaktirajte me direktno na email: \nvisnjicmatej@gmail.com"
         );
       }
     );
